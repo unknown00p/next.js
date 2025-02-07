43c83a34c27a91ae670511159584e7b72445439e
unknown00p <rajpoint8976@gmail.com>
2025-02-07 21:27:12 +0530

    commit

--- src/app/backend/hello/route.ts
+++ src/app/backend/hello/route.ts
@@ -1,7 +1,11 @@
+import { NextRequest } from "next/server";
 import { data } from "./data";
 
-export async function GET() {
-  return Response.json(data);
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const quary = searchParams.get('query')
+  const filterdComments = quary ? data.filter((val)=> val.text.includes(quary)): data 
+  return Response.json(filterdComments);
 }
 
 export async function POST(request: Request) {
@@ -14,6 +18,6 @@ export async function POST(request: Request) {
   data.push(newData);
   return new Response(JSON.stringify(newData), {
     headers: { "Content-Type": "application/json" },
-    status: 201
+    status: 201,
   });
 }
